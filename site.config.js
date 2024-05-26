9c5d5d3d16918f928909bdc90f5e76a5b536770b
the108matrix <162987166+the108matrix@users.noreply.github.com>
2024-05-26 10:37:07 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Aaina",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Editor & Researcher",
+    bio: "Keen about all things natural",
+    email: "the108matrix@gmail.com",
+    github: "the108matrix",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Bee Raw`,
+      href: "https://github.com/the108matrix/beeraw",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "beeraw",
+    description: "welcome to Bee Raw!",
   },
 
   // CONFIG configration (required)
@@ -53,7 +52,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
